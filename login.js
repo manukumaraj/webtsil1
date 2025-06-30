9d37c09850f7c8233aecc4cfe70316c3434421cf
Manukumar <144512334+manukumaraj@users.noreply.github.com>
2025-06-30 16:34:15 +0530

    Update login.js

--- login.js
+++ login.js
@@ -14,7 +14,8 @@ const userGroups = {
     users13: { redirect: "unit_ppd.html", users: [{ username: "ppd", password: "ppd123" }] },
     users14: { redirect: "kpi_data.html", target: "_blank", users: [{ username: "kpi", password: "kpi123" }] },
     users15: { redirect: "unit_cmp.html", users: [{ username: "cmp", password: "cmp988" }] },
-    users16: { redirect: "unit_mrh.html", users: [{ username: "mrh", password: "mrh159" }] }
+    users16: { redirect: "unit_mrh.html", users: [{ username: "mrh", password: "mrh159" }] },
+    users16: { redirect: "main_ho_test.html", users: [{ username: "man", password: "man2975" }] },
 };
 
 // Login function
